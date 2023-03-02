47f6ef9070e4e3e5ff072c12f4f6975f55755533
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-03-02 20:17:25 +0600

    redact html files

--- src/model/CandidateGeneration.java
+++ src/model/CandidateGeneration.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class candidateGeneration {
+public class CandidateGeneration {
     private List<Candidate> candidates = new ArrayList<>();
 
     public List<Candidate> getCandidates() {
