1c57a0f9128c1dbafa5d04acdf303f9562b078c7
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-03-04 10:47:48 +0600

    add new classes

--- src/model/CandidateGeneration.java
+++ src/model/CandidateGeneration.java
@@ -1,5 +1,7 @@
 package model;
 
+import FileService.FileService;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,4 +15,12 @@ public class CandidateGeneration {
     public void setCandidates(List<Candidate> candidates) {
         this.candidates = candidates;
     }
-}
+    public CandidateGeneration(){
+        this.candidates = FileService.readFile();
+        for(var candidate: candidates){
+            candidate.setId(candidates.indexOf(candidate)+ 1);
+            candidate.setVotes(0);
+        }
+    }
+    }
+
--- src/model/Lesson48Server.java
+++ src/model/Lesson48Server.java
@@ -8,19 +8,27 @@ import freemarker.template.TemplateExceptionHandler;
 import server.BasicServer;
 import server.ContentType;
 import server.ResponseCodes;
+import server.Utils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class Lesson48Server extends BasicServer {
     private final static Configuration freemarker = initFreeMarker();
+    private static CandidateGeneration candidates = new CandidateGeneration();
+    private static Candidate currentVotedCandidate = new Candidate();
+    private static Integer allVotes = 0;
     public Lesson48Server(String host, int port) throws IOException {
         super(host, port);
         registerGet("/", this ::candidateRender);
-        registerGet("/thankYou", this::thankYouRender);
+        registerGet("/thankyou", this::thankYouRender);
         registerPost("/vote", this :: voteRender);
         registerGet("/votes", this :: votesRender);
     }
@@ -41,7 +49,6 @@ public class Lesson48Server extends BasicServer {
     }
     protected void renderTemplate(HttpExchange exchange, String templateFile, Object dataModel) {
         try {
-
             Template temp = freemarker.getTemplate(templateFile);
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
             try (OutputStreamWriter writer = new OutputStreamWriter(stream)) {
@@ -53,18 +60,36 @@ public class Lesson48Server extends BasicServer {
         } catch (IOException | TemplateException e) {
             e.printStackTrace();
         }
+
     }
     private void thankYouRender(HttpExchange exchange) {
-        renderTemplate(exchange, "candidate.html", getBooksDataModel());
+        Map<String, Object> data = new HashMap<>();
+        data.put("candidate", currentVotedCandidate);
+        data.put("percent", (currentVotedCandidate.getVotes() * 100 / allVotes));
+        renderTemplate(exchange, "thankyou.html", data);
     }
     private void voteRender(HttpExchange exchange) {
-        renderTemplate(exchange, "votes.html", getBooksDataModel());
+        String raw = getBody(exchange);
+        Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
+        for(var candidate: candidates.getCandidates()){
+        if(parsed.get("candidateId").equalsIgnoreCase(candidate.getId().toString())){
+           candidate.setVotes(candidate.getVotes() + 1);
+           currentVotedCandidate = candidate;
+           break;
+            }
+        }
+        allVotes++;
+        redirect303(exchange, "/thankyou");
+
     }
     private void votesRender(HttpExchange exchange) {
-        renderTemplate(exchange, "votes.html", getBooksDataModel());
+        Map<String, Object> data = new HashMap<>();
+        data.put("allvotes", allVotes);
+        data.put("candidates", candidates.getCandidates().stream().sorted((Comparator.comparing(Candidate::getVotes)).reversed()).collect(Collectors.toList()));
+        renderTemplate(exchange, "votes.html", data);
     }
     private void candidateRender(HttpExchange exchange) {
-        renderTemplate(exchange, "votes.html", getSampleDataModel());
+      renderTemplate(exchange, "candidates.html", candidates);
     }
 }
 
