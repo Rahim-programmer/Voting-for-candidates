e9d4fdebcd0691a03200ee96f8e500ef3d955e10
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-03-04 10:48:09 +0600

    run the project

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-    try {
-        new Lesson48Server("localhost", 8965).start()
-    }catch (IOException ex){
-        ex.printStackTrace();
+        try {
+            new Lesson48Server("localhost", 8965).start();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
